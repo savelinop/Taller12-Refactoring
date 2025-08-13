1e9e4d34babf85b0952c3e8436ec47e6ff0fa2da
savelinop <savelino@espol.edu.ec>
2025-08-13 14:42:37 -0500

    2 cambios

--- Taller Refactoring/Taller Refactoring/src/Empleado.java
+++ Taller Refactoring/Taller Refactoring/src/Empleado.java
@@ -14,21 +14,6 @@ public class Empleado {
         this.departamento = departamento;
     }
 
-    public double calcularSalario() {
-        if (salarioBase>0) {
-            if (horasTrabajadas >= 0) {
-                // Horas trabajadas normales = 40;
-                if (horasTrabajadas > 40) {
-                    salarioBase += (horasTrabajadas - 40) * 50; // Pago de horas extra
-                }
-            }else {
-                throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
-            }
-        } else {
-            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
-        }
-        return salarioBase;
-    }
 
     public String getNombre() {
         return nombre;
@@ -46,7 +31,7 @@ public class Empleado {
         this.salarioBase = salarioBase;
     }
 
-    public int getHorasTrabajadas() {
+    public double getHorasTrabajadas() {
         return horasTrabajadas;
     }
 
--- Taller Refactoring/Taller Refactoring/src/EmpleadoPorHoras.java
+++ Taller Refactoring/Taller Refactoring/src/EmpleadoPorHoras.java
@@ -1,6 +1,6 @@
 public class EmpleadoPorHoras extends Empleado {
     private String genero;
-
+    private double salarioBase;
     public EmpleadoPorHoras(String nombre, int horasTrabajadas, double tarifaHora, String departamento, String genero) {
         super(nombre, 0, horasTrabajadas, tarifaHora, departamento);
         super.setSalarioBase(calcularSalario());
@@ -15,10 +15,23 @@ public class EmpleadoPorHoras extends Empleado {
         System.out.println("Departamento: " + super.getDepartamento());
     }
 
-    @Override
+    
+
     public double calcularSalario() {
-        double salario = super.getHorasTrabajadas() * super.getTarifaHora();
-        return salario;
+        
+        if (getSalarioBase()>0) {
+            if (getHorasTrabajadas() >= 0) {
+                // Horas trabajadas normales = 40;
+                if (getHorasTrabajadas()> 40) {
+                    salarioBase += (getHorasTrabajadas() - 40) * 50; // Pago de horas extra
+                }
+            }else {
+                throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
+            }
+        } else {
+            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
+        }
+        return salarioBase;
     }
 
     // Más metodos
